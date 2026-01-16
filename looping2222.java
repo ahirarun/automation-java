e41384785ca27bf99c12359979f97cb7f6d36bb4
Arun Ahir <ahirarun845@gmail.com>
2026-01-16 15:25:01 +0530

    Add files via upload

---
+++ looping2222.java
@@ -0,0 +1,17 @@
+package test;
+
+public class looping2222 {
+	
+	public static void main(String[] args) {
+		
+		
+		  int i=51;
+	      while(i<=60)
+	    	  
+	    	  
+	      {
+        System.out.println(i);
+        i++;
+}
+}
+}
