3349cb96027166dc6e5976396efafe968deba770
Arun Ahir <ahirarun845@gmail.com>
2026-01-16 15:07:47 +0530

    Add files via upload

---
+++ positivenagative.java
@@ -0,0 +1,29 @@
+
+package test;
+
+import java.util.Scanner;
+
+public class positivenagative {
+	
+	public static void main(String[] args) {
+		 Scanner Sc = new Scanner(System.in);
+		 
+		 System.out.println(45);
+		 int  num = Sc.nextInt();
+		 if (num>0)
+		 {
+		   System.out.println("number is positive");
+		 }
+		 else if (num <0)
+		 {
+			 System.out.println("number is nagative");
+		 }
+         else	 
+         {
+         System.out.println("number is zero");
+         }
+             
+	}
+
+}
+	
---
+++ prime.java
@@ -0,0 +1,32 @@
+package test;
+import java.util.Scanner;
+
+public class prime {
+	public static void main(String[] args) {
+		
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int num = Sc.nextInt ();
+		
+		switch(num) {
+	
+		case 1:System.out.println("monday");
+		break;
+		
+		case 2:System.out.println("tuesday");
+		break;
+	
+		case 3:System.out.println("wednesday");
+		break;
+		
+		case 4:System.out.println("thurseday");
+		break;
+		
+		case 5:System.out.println("freiday");
+		break;
+		
+		case 6:System.out.println("saturday");
+		break;
+	}
+}	
+}
