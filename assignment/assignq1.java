d1fe43aa0d43bab6daf991b684f83fa22dae3bc1
Arun Ahir <ahirarun845@gmail.com>
2026-02-19 15:22:33 +0530

    Add files via upload

---
+++ assignment/assignq1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class assignq1 
+{
+     
+     public static void main(String[] args)
+    {
+		int i = 1;
+	    while (i<=10) {
+        System.out.println(i);
+        i++;
+       }
+    }
+}
